83f7b2eb0e2419e5eaad27504010f7a4ae24f781
rohit9852 <rohitpr9852@gmia.com>
2021-02-27 15:37:02 +0530

    first commit

---
+++ src/component/addTask.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';  
+import "react-toastify/dist/ReactToastify.min.css";
+import './addTask.css';
+
+export default function({addTask}) {
+
+    const [ userInput, setUserInput ] = useState('');
+    const [ date, setDate ] = useState('');
+    const [ userAssigned, setUserAssigned ] = useState('');
+    const [ error, setError ]  = useState('')
+
+    const handleCAssigeehange = (e) => {
+        setUserAssigned(e.currentTarget.value)
+    }
+
+    const handleTaskChange = (e) => {
+        setUserInput(e.currentTarget.value)
+    }
+
+    const handleDateChange = (e) => {
+        setDate(e.currentTarget.value);
+    }
+
+    toast.configure()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(userInput.length!= 0 && date.length!= 0 && userAssigned.length!=0) {
+        addTask(userInput,date,userAssigned);
+        toast('Task Added Succesfully',{
+            position: toast.POSITION.TOP_CENTER
+        })
+        } else {
+            setError('All data are required.')
+        }
+        setUserInput("");
+        setDate('');
+        setUserAssigned('')
+    }
+
+    return(
+        <div className='addWrapper'>
+            <h2 className='titleAdd'>Add Task</h2>
+        
+            <form onSubmit={handleSubmit} className='formWraper'>
+            <label>Task Name</label>
+                <input value={userInput} type="text" onChange={handleTaskChange} className='taskName'/>
+                <label>Date</label>
+                <input type='Date'value={date} onChange={handleDateChange} className='date' />
+                <label>Assigned To</label>
+                <input value={userAssigned} type="text" onChange={handleCAssigeehange} className='assignee'/>
+                <button className='btn'>Submit</button>
+                <p className='errMessage'>{error}</p>
+            </form>
+        </div>
+    )
+}
---
+++ src/component/layout.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import AddTask from './addTask';
+import TaskList from './taskList';
+import {data} from '../constant/existingData';
+import { toast } from 'react-toastify';  
+import "react-toastify/dist/ReactToastify.min.css";
+import './layout.css';
+
+export default function Layout() {
+    const [ toDoList, setToDoList ] = useState(data);
+    toast.configure()
+    const addTask = (userInput,date,userAssigned ) => {
+      let copy = [...toDoList];
+      let temp = { id:toDoList.length+1,name: userInput, date: date, assigned:userAssigned}
+      copy.unshift(temp);
+      setToDoList(copy);
+    }
+    const onRemove = id => e => {
+      setToDoList(toDoList.filter(todo => todo.id !== id));
+      toast('Task remove Succesfully',{
+          position: toast.POSITION.TOP_CENTER
+      })
+    };
+
+    return (
+        <div className='layoutWraper'>
+            <div className="headerBar">
+                <h1 className='title'>Task Manger</h1>
+                <h4 className='version'>v1.0</h4>
+            </div>
+            <div className='gg'>
+                <AddTask 
+                addTask={addTask}
+                />
+                <TaskList
+                toDoList={toDoList}
+                onRemove={onRemove}
+                />
+            </div>
+        </div>
+    )
+}
